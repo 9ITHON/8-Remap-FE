15bb05ff6dee045d3b93124aeaee9f3a3de43a45
moridodos <coderdhk06@gmail.com>
2025-07-13 04:40:45 +0900

    feat: map complete2

--- app/components/map/mapContainer.tsx
+++ app/components/map/mapContainer.tsx
@@ -1,14 +1,25 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
+import { exampleMemory, MemorySchema } from './schema';
+import BottomSheet from './bottomSheet';
+import { useUploadStore } from '@/store/useUploadStore';
+import { useRouter } from 'next/navigation';
 
-import { exampleMemory } from './schema';
-
-export default function MapContainer() {
-  const [title, setTitle] = useState('');
-  const [userName, setUserName] = useState('');
-  const [createdAt, setCreatedAt] = useState('');
+export default function MapContainer({ address }: { address?: string }) {
+  const [popSheet, setPopSheet] = useState(false);
+  const [re, setRe] = useState<number[] | null>(null);
+  const [pieces, setPieces] = useState<MemorySchema[]>(exampleMemory);
+  const router = useRouter();
+  const { isUploadMode } = useUploadStore();
+  const uploadModeRef = useRef(isUploadMode);
+  const mapRef = useRef<naver.maps.Map | null>(null);
 
   useEffect(() => {
+    const map = new naver.maps.Map('map', {
+      center: new naver.maps.LatLng(37.5666805, 126.9784147),
+      zoom: 15,
+    });
+
     const checkAndInit = () => {
       if (!naver || !naver.maps || !naver.maps.Service) {
         console.warn('naver.maps.Service is not fully ready yet! Retrying...');
@@ -20,10 +31,7 @@ export default function MapContainer() {
     };
 
     const tryMapInit = () => {
-      const map = new naver.maps.Map('map', {
-        center: new naver.maps.LatLng(37.5666805, 126.9784147),
-        zoom: 15,
-      });
+      mapRef.current = map;
 
       const initGeolocation = () => {
         const onSuccessGeolocation = (position: GeolocationPosition) => {
@@ -40,7 +48,6 @@ export default function MapContainer() {
 
         const onErrorGeolocation = () => {
           const center = map.getCenter();
-
           console.warn('Geolocation failed, using default center:', center);
         };
 
@@ -50,44 +57,74 @@ export default function MapContainer() {
             onErrorGeolocation
           );
         }
+        setPopSheet(false);
       };
       initGeolocation();
 
-      exampleMemory.map(
-        ({ id, title, userName, coordinates, createdAt, isPublic }) => {
-          const marker = new naver.maps.Marker({
-            position: new naver.maps.LatLng(coordinates.x, coordinates.y),
-            map: map,
-            title: `${title} by ${userName}`,
-            icon: {
-              content: [
-                '<img src="/images/marker.png" alt="marker" ' +
-                  'style="margin: 0px; padding: 0px; border: 0px solid transparent; display: block; max-width: none; max-height: none; ' +
-                  '-webkit-user-select: none; width: 33px; height: 59px; left: 0px; top: 0px;"></img>',
-                '<div class="relative ml-[50%]">',
-                '<div class="flex flex-col items-center -translate-x-1/2 absolute text-[12px] text-shadow max-w-[120px] min-w-[70px] break-words break-keep text-center">',
-                `<div class="absolute text-shadow rounded-[15px]">${title}</div>`,
-                '</div>',
-                '</div>',
-              ].join(''),
-              size: new naver.maps.Size(33, 59),
-              anchor: new naver.maps.Point(17, 59),
-            },
-          });
-
-          if (isPublic) {
-            console.log(`Public memory added: ${title}`);
-          }
-
-          naver.maps.Event.addListener(marker, 'click', () => {
-            console.log(`Marker clicked: ${id} - ${title}`);
-          });
-        }
+      const grouped = exampleMemory.reduce<Record<string, MemorySchema[]>>(
+        (acc, mem) => {
+          const key = `${mem.coordinates.x},${mem.coordinates.y}`;
+          if (!acc[key]) acc[key] = [];
+          acc[key].push(mem);
+          return acc;
+        },
+        {}
       );
+
+      Object.entries(grouped).forEach(([key, memories]) => {
+        const { x, y } = memories[0].coordinates;
+
+        const marker = new naver.maps.Marker({
+          position: new naver.maps.LatLng(x, y),
+          map: map,
+          title: `${memories.length} memories`,
+          icon: {
+            content: [
+              '<img src="/images/marker.png" alt="marker" ' +
+                'style="margin: 0px; padding: 0px; border: 0px solid transparent; display: block; max-width: none; max-height: none; ' +
+                '-webkit-user-select: none; width: 33px; height: 59px; left: 0px; top: 0px;"></img>',
+              '<div class="relative ml-[50%]">',
+              '<div class="flex flex-col items-center -translate-x-1/2 absolute text-[12px] text-shadow max-w-[120px] min-w-[70px] break-words break-keep text-center">',
+              `<div class="absolute text-shadow rounded-[15px]">${memories.length}개의 기억</div>`,
+              '</div>',
+              '</div>',
+              ,
+            ].join(''),
+            size: new naver.maps.Size(33, 59),
+            anchor: new naver.maps.Point(17, 59),
+          },
+        });
+
+        naver.maps.Event.addListener(marker, 'click', () => {
+          console.log(`Marker clicked at ${key}`);
+          console.log('Memories at this location:', memories);
+          setPieces(memories); // Update the pieces state with memories at this location
+          setPopSheet(true);
+        });
+      });
+
+      interface NaverMapMouseEvent {
+        coord: naver.maps.LatLng;
+        point: naver.maps.Point;
+        domEvent: MouseEvent;
+      }
+
+      naver.maps.Event.addListener(map, 'click', (e: NaverMapMouseEvent) => {
+        console.log('Map clicked at:', e.coord);
+        setPopSheet(false);
+        // console.log(isUploadMode);
+        if (uploadModeRef.current) {
+          if (confirm('이 위치에 업로드 하시겠습니까?')) {
+            const lat = e.coord.y;
+            const lng = e.coord.x;
+            router.push(`/upload?lat=${lat}&lng=${lng}`);
+          }
+        }
+      });
 
       naver.maps.Event.once(map, 'init', () => {
         const html = `
-          <a href="#" class="flex justify-center items-center bg-white p-[1rem] rounded-full m-[1.5rem] active:bg-gray-300">
+          <a href="#" class="flex justify-center items-center bg-white p-[1rem] rounded-full m-[1.5rem] active:bg-gray-300 box-shadow">
             <img src="/images/point.png"></img>
           </a>`;
         console.log('Map initialized successfully');
@@ -108,33 +145,37 @@ export default function MapContainer() {
       });
     };
 
-    // const tryGeocode = (retryCount = 0) => {
-    //   if (retryCount > 3) {
-    //     console.error('Geocode failed after multiple retries.');
-    //     return;
-    //   }
-
-    //   naver.maps.Service.reverseGeocode(
-    //     { coords: new naver.maps.LatLng(37.3595316, 127.1052133) },
-    //     function (status, response) {
-    //       if (status !== naver.maps.Service.Status.OK) {
-    //         console.error('Geocode error:', response, 'Retrying...');
-    //         setTimeout(() => tryGeocode(retryCount + 1), 500);
-    //         return;
-    //       }
-
-    //       const result = response.v2;
-    //       const items = result.results;
-    //       const address = result.address;
-
-    //       console.log('Geocode successful:', items, address);
-    //       initMap(address.jibunAddress || address.roadAddress);
-    //     }
-    //   );
-    // };
-
     checkAndInit();
   }, []);
+
+  useEffect(() => {
+    if (!address || !mapRef.current) return;
+
+    naver.maps.Service.geocode({ query: address }, function (status, response) {
+      if (status !== naver.maps.Service.Status.OK) {
+        console.error('Geocode error:', response);
+        return;
+      }
+
+      const result = response.v2;
+      const items = result.addresses;
+
+      if (items.length === 0) {
+        console.warn('No results found for the address:', address);
+        return;
+      }
+
+      const remap = new naver.maps.LatLng(
+        Number(items[0].y),
+        Number(items[0].x)
+      );
+      mapRef.current!.setCenter(remap);
+    });
+  }, [address]);
+
+  useEffect(() => {
+    uploadModeRef.current = isUploadMode;
+  }, [isUploadMode]);
 
   return (
     <>
@@ -145,6 +186,7 @@ export default function MapContainer() {
           style={{ height: 'calc(100% - 7.9rem)' }}
         ></div>
       </div>
+      {popSheet && <BottomSheet arr={pieces} />}
     </>
   );
 }
--- app/components/map/schema.ts
+++ app/components/map/schema.ts
@@ -1,4 +1,4 @@
-interface MemorySchema {
+export interface MemorySchema {
   id: string; // 기억의 고유 랜덤 ID
   title: string;
   userName: string; // id에 해당하는 유저의 이름
@@ -9,7 +9,6 @@ interface MemorySchema {
   createdAt: string; // 기억이 생성된 날짜,
   isPublic: boolean; // 기억의 공개 여부
 }
-
 export const exampleMemory: MemorySchema[] = [
   {
     id: 'mem0',
@@ -19,84 +18,183 @@ export const exampleMemory: MemorySchema[] = [
     createdAt: '2023-10-01T12:00:00Z',
     isPublic: true,
   },
+  // 좌표 1번 (2개)
   {
-    id: 'mem1',
+    id: 'mem1-1',
     title: '1번째 기억',
     userName: '유저1',
-    coordinates: { x: 37.41, y: 127.11 }, // 북동쪽
+    coordinates: { x: 37.41, y: 127.11 },
     createdAt: '2023-10-01T12:00:00Z',
     isPublic: true,
   },
   {
-    id: 'mem2',
+    id: 'mem1-2',
     title: '2번째 기억',
     userName: '유저2',
-    coordinates: { x: 37.394, y: 127.091 }, // 남서쪽
+    coordinates: { x: 37.41, y: 127.11 },
     createdAt: '2023-10-02T12:00:00Z',
-    isPublic: false,
+    isPublic: true,
   },
+
+  // 좌표 2번 (3개)
   {
-    id: 'mem3',
+    id: 'mem2-1',
     title: '3번째 기억',
     userName: '유저3',
-    coordinates: { x: 37.409, y: 127.088 }, // 남동쪽
+    coordinates: { x: 37.394, y: 127.091 },
     createdAt: '2023-10-03T12:00:00Z',
     isPublic: true,
   },
   {
-    id: 'mem4',
+    id: 'mem2-2',
     title: '4번째 기억',
     userName: '유저4',
-    coordinates: { x: 37.39, y: 127.112 }, // 북서쪽
+    coordinates: { x: 37.394, y: 127.091 },
     createdAt: '2023-10-04T12:00:00Z',
     isPublic: true,
   },
   {
-    id: 'mem5',
+    id: 'mem2-3',
     title: '5번째 기억',
     userName: '유저5',
-    coordinates: { x: 37.393, y: 127.107 },
+    coordinates: { x: 37.394, y: 127.091 },
     createdAt: '2023-10-05T12:00:00Z',
-    isPublic: false,
+    isPublic: true,
   },
+
+  // 좌표 3번 (1개)
   {
-    id: 'mem6',
+    id: 'mem3-1',
     title: '6번째 기억',
     userName: '유저6',
-    coordinates: { x: 37.411, y: 127.09 },
+    coordinates: { x: 37.409, y: 127.088 },
     createdAt: '2023-10-06T12:00:00Z',
     isPublic: true,
   },
+
+  // 좌표 4번 (4개)
   {
-    id: 'mem7',
+    id: 'mem4-1',
     title: '7번째 기억',
     userName: '유저7',
-    coordinates: { x: 37.3905, y: 127.095 },
+    coordinates: { x: 37.39, y: 127.112 },
     createdAt: '2023-10-07T12:00:00Z',
     isPublic: true,
   },
   {
-    id: 'mem8',
+    id: 'mem4-2',
     title: '8번째 기억',
     userName: '유저8',
-    coordinates: { x: 37.412, y: 127.113 },
+    coordinates: { x: 37.39, y: 127.112 },
     createdAt: '2023-10-08T12:00:00Z',
-    isPublic: false,
+    isPublic: true,
   },
   {
-    id: 'mem9',
+    id: 'mem4-3',
     title: '9번째 기억',
     userName: '유저9',
-    coordinates: { x: 37.389, y: 127.102 },
+    coordinates: { x: 37.39, y: 127.112 },
     createdAt: '2023-10-09T12:00:00Z',
     isPublic: true,
   },
   {
-    id: 'mem10',
+    id: 'mem4-4',
     title: '10번째 기억',
     userName: '유저10',
-    coordinates: { x: 37.413, y: 127.108 },
+    coordinates: { x: 37.39, y: 127.112 },
     createdAt: '2023-10-10T12:00:00Z',
-    isPublic: false,
+    isPublic: true,
+  },
+
+  // 좌표 5번 (1개)
+  {
+    id: 'mem5-1',
+    title: '11번째 기억',
+    userName: '유저11',
+    coordinates: { x: 37.393, y: 127.107 },
+    createdAt: '2023-10-11T12:00:00Z',
+    isPublic: true,
+  },
+
+  // 좌표 6번 (2개)
+  {
+    id: 'mem6-1',
+    title: '12번째 기억',
+    userName: '유저12',
+    coordinates: { x: 37.411, y: 127.09 },
+    createdAt: '2023-10-12T12:00:00Z',
+    isPublic: true,
+  },
+  {
+    id: 'mem6-2',
+    title: '13번째 기억',
+    userName: '유저13',
+    coordinates: { x: 37.411, y: 127.09 },
+    createdAt: '2023-10-13T12:00:00Z',
+    isPublic: true,
+  },
+
+  // 좌표 7번 (3개)
+  {
+    id: 'mem7-1',
+    title: '14번째 기억',
+    userName: '유저14',
+    coordinates: { x: 37.3905, y: 127.095 },
+    createdAt: '2023-10-14T12:00:00Z',
+    isPublic: true,
+  },
+  {
+    id: 'mem7-2',
+    title: '15번째 기억',
+    userName: '유저15',
+    coordinates: { x: 37.3905, y: 127.095 },
+    createdAt: '2023-10-15T12:00:00Z',
+    isPublic: true,
+  },
+  {
+    id: 'mem7-3',
+    title: '16번째 기억',
+    userName: '유저16',
+    coordinates: { x: 37.3905, y: 127.095 },
+    createdAt: '2023-10-16T12:00:00Z',
+    isPublic: true,
+  },
+
+  // 좌표 8번 (1개)
+  {
+    id: 'mem8-1',
+    title: '17번째 기억',
+    userName: '유저17',
+    coordinates: { x: 37.412, y: 127.113 },
+    createdAt: '2023-10-17T12:00:00Z',
+    isPublic: true,
+  },
+
+  // 좌표 9번 (2개)
+  {
+    id: 'mem9-1',
+    title: '18번째 기억',
+    userName: '유저18',
+    coordinates: { x: 37.389, y: 127.102 },
+    createdAt: '2023-10-18T12:00:00Z',
+    isPublic: true,
+  },
+  {
+    id: 'mem9-2',
+    title: '19번째 기억',
+    userName: '유저19',
+    coordinates: { x: 37.389, y: 127.102 },
+    createdAt: '2023-10-19T12:00:00Z',
+    isPublic: true,
+  },
+
+  // 좌표 10번 (1개)
+  {
+    id: 'mem10-1',
+    title: '20번째 기억',
+    userName: '유저20',
+    coordinates: { x: 37.413, y: 127.108 },
+    createdAt: '2023-10-20T12:00:00Z',
+    isPublic: true,
   },
 ];
